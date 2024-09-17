6c7969a9b976757ed937c89098de4acd25d33a6e
runshuvo <nhshuvo2003@gmail.com>
2024-09-17 17:18:54 +0600

    second

---
+++ scrpt.js
@@ -0,0 +1,6 @@
+function changingPage(){
+    const page1=document.getElementById('page1');
+    page1.classList.add('hidden');
+     const page2=document.getElementById('page2');
+     page2.classList.remove('hidden');
+}
